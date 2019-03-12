5836b30f17e51df3662ea89c993ff07b566bae31
Dave Rigby <daver@couchbase.com>
2019-03-12 11:08:23 +0000

    MB-22008: Add ASan-UBSan support for non-kv_engine
    Move the config for ASan-UBSan to config/common/ASan-UBSan.groovy so
    it can be used by other non- kv_engine projects (e.g. couchstore).
    
    Change-Id: Ia408b12078f8e7de7f6702c6dd349e8f2eb469dc
    Reviewed-on: http://review.couchbase.org/106043
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Dave Rigby <daver@couchbase.com>

--- jenkins-jobs/config/kv_engine/ASan-UBSan.groovy
+++ jenkins-jobs/config/kv_engine/ASan-UBSan.groovy
@@ -1,23 +1,4 @@
 return {
-// 2018-02-01: cbdeps are now compiled with GCC 7
-// (see https://issues.couchbase.com/browse/CBD-2151).
-// We therefore need to ensure that clang uses GCC7's libstdc++
-// (and not the default GCC 5). As GCC 7 is installed in /usr/local,
-// clang doesn't automatically detect it, so we need to explicitly
-// tell it to use the toolchain in /usr/local.
-CMAKE_ARGS="-DCMAKE_BUILD_TYPE=Debug -DCOUCHBASE_KV_COMMIT_VALIDATION=1 " +
-"-DCMAKE_C_COMPILER_EXTERNAL_TOOLCHAIN=/usr/local " +
-"-DCMAKE_CXX_COMPILER_EXTERNAL_TOOLCHAIN=/usr/local"
-
-ENABLE_ADDRESSSANITIZER=1
-ENABLE_UNDEFINEDSANITIZER=1
-UBSAN_OPTIONS="print_stacktrace=1"
-ASAN_OPTIONS="abort_on_error=true disable_coredump=0 use_madv_dontdump=1"
-
-// 2017-05-26: ubuntu16.04 machines are currently 4 cores with 8GB RAM -
-// constrain parallelism so we don't swap (too much)...
-PARALLELISM=6
-TEST_PARALLELISM=4
 
 // Breakpad disabled as it deliberately crashes (which ASan doesn't like :)
 // breakdancer disabled as it is very slow under ASan.
