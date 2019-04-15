8fa714efc8dcfc10583e84034238cf24a7362f1f
Dave Rigby <daver@couchbase.com>
2019-04-15 17:37:31 +0100

    Revert "Re-enable rocksdb tests under ASan"
    A number of the RocksDB tests intermittently fail under ASan/UBSan
    with an error along the lines of:
    
        Uncaught std::exception. what():RocksDBKVStore::openDB: Open
        failed for database
        './ep_testsuite.full_eviction.rocksdb/rocksdb.0': While lock file:
        ./ep_testsuite.full_eviction.rocksdb/rocksdb.0/LOCK: No locks
        available
    
    Given RocksDB is currently experimental, just disable these tests to
    reduce the noise in CV.
    
    This reverts commit d5dfcf72c7bdeefee4287e3a1d365e113eb1801f.
    
    Change-Id: Id5007a2462c5c1408871d519be1be512fbf33b37
    Reviewed-on: http://review.couchbase.org/107824
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Dave Rigby <daver@couchbase.com>

--- jenkins-jobs/config/kv_engine/ASan-UBSan.groovy
+++ jenkins-jobs/config/kv_engine/ASan-UBSan.groovy
@@ -2,5 +2,15 @@ return {
 
 // Breakpad disabled as it deliberately crashes (which ASan doesn't like :)
 // breakdancer disabled as it is very slow under ASan.
-TESTS_EXCLUDE="memcached-breakpad-test-segfault|breakdancer"
+// MB-25989: Disabling rocksdb tests while they are not stable.
+TESTS_EXCLUDE="memcached-breakpad-test-segfault|breakdancer|" +
+"ep_testsuite_basic.value_eviction.rocksdb|" +
+"ep_testsuite_basic.full_eviction.rocksdb|" +
+"ep_testsuite.value_eviction.rocksdb|" +
+"ep_testsuite.full_eviction.rocksdb|" +
+"ep_testsuite_dcp.full_eviction.rocksdb|" +
+"ep_testsuite_dcp.value_eviction.rocksdb|" +
+"ep_testsuite_xdcr.full_eviction.rocksdb|" +
+"ep_testsuite_xdcr.value_eviction.rocksdb|" +
+"ep-engine-persistence-unit-tests.rocksdb"
 }
