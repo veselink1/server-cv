45f088a9c8ce3f9590cb0c7025f444ffd3c2e826
Dave Rigby <daver@couchbase.com>
2020-03-25 11:03:39 +0000

    MB-37898: Use XCode 11.3.1 for C++17 support
    For C++17 support we require at least XCode 11. This is installed in a
    non-default path on the macOS CV machines.
    
    Change-Id: I95b04f69914df13748516bd4cd97e58da90c29b4
    Reviewed-on: http://review.couchbase.org/124248
    Tested-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Daniel Owen <owend@couchbase.com>

--- jenkins-jobs/config/common/macos.groovy
+++ jenkins-jobs/config/common/macos.groovy
@@ -1,3 +1,7 @@
 return {
-CMAKE_ARGS= "${CMAKE_ARGS} -DBUILD_ENTERPRISE=1"
+    CMAKE_ARGS= "${CMAKE_ARGS} -DBUILD_ENTERPRISE=1"
+
+    // For C++17 support we require at least XCode 11. This is installed in
+    // a non-default path on the macOS CV machines.
+    PATH="/Library/Developer/CommandLineTools_11.3.1/usr/bin:${PATH}"
 }
