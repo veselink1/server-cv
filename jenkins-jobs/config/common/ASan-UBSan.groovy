94b2b1e44247c5c5f60b3d59e5e18dfaa4bb55f7
Dave Rigby <daver@couchbase.com>
2021-04-08 09:44:35 +0100

    MB-45044: Use gcc-10's C++ standard library for clang
    As of Cheshire-Cat we have switched to GCC 10, which is installed in
    parallel with GCC-7 on the standard build image. To use the newer C++
    standard library from GCC-10 we need to explicitly tell clang where to
    find it.
    
    Change-Id: I360f178035de065dd5bbf194bcd9bd927eabac3d
    Reviewed-on: http://review.couchbase.org/c/server-cv/+/150744
    Tested-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@couchbase.com>

--- jenkins-jobs/config/common/ASan-UBSan.groovy
+++ jenkins-jobs/config/common/ASan-UBSan.groovy
@@ -1,6 +1,13 @@
 return {
     CC="clang-9"
     CXX="clang++-9"
+    // As of Cheshire-Cat we have switch to GCC 10, which is installed in
+    // parallel with GCC-7 on the standard build image. To use the newer C++
+    // standard library from GCC-10 we need to explicitly tell clang where to
+    // find it.
+    CFLAGS="--gcc-toolchain=/opt/gcc-10.2.0 -Wl,-rpath,/opt/gcc-10.2.0/lib64"
+    CXXFLAGS="--gcc-toolchain=/opt/gcc-10.2.0 -Wl,-rpath,/opt/gcc-10.2.0/lib64"
+
     // Add llvm-9's bin dir to path, so `llvm-symbolizer' can be found (we
     // cannot use the versioned suffix one - llvm-symbolizer-9' - as the
     // binary must be named 'llvm-symbolizer').
