97df8ea054bdc1f4827759228154f9c7d343503c
Dave Rigby <daver@couchbase.com>
2021-07-07 10:09:12 +0100

    libFuzzer: Only run fuzz tests
    There's no point in running non-fuzzer tests in this config - they
    won't do anything which isn't already done in the ASan-UBSan tests.
    
    Change-Id: I1f536aaa1ce04526e759f35e8749b57ab72b7aef
    Reviewed-on: http://review.couchbase.org/c/server-cv/+/157092
    Tested-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@couchbase.com>

--- jenkins-jobs/config/common/libFuzzer.groovy
+++ jenkins-jobs/config/common/libFuzzer.groovy
@@ -19,4 +19,6 @@ return {
     // precompiled-headers as using them at the same time as libFuzzer fails:
     //     error: current translation unit is compiled with the target feature '-fsanitize=fuzzer-no-link' but the AST file was not
     CMAKE_ARGS="${CMAKE_ARGS} -DCB_LIBFUZZER=1 -DCB_ADDRESSSANITIZER=1 -DCB_UNDEFINEDSANITIZER=1 -DCB_PCH=0"
+
+    CTEST_ARGS="--tests-regex fuzz"
 }
