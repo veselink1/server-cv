285a42bf65596d4f8eea86b4dcb82fde8e0752d2
Dave Rigby <daver@couchbase.com>
2022-04-19 14:34:39 +0100

    Revert "couchstore/ASan-UBSan: Update warnings threshold"
    Now a suppressions file is being used for UBSan which filters out the
    known vptr issue in V* code, we can revert to expecting zero warnings.
    
    This reverts commit 833ff3e6cca79bffb080943b72b968be3b342d10.
    
    Change-Id: I0d11016825bec8899e323a1b9eca3e13d30f80ab
    Reviewed-on: https://review.couchbase.org/c/server-cv/+/173822
    Tested-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Paolo Cocchi <paolo.cocchi@couchbase.com>

--- jenkins-jobs/config/couchstore/ASan-UBSan.groovy
+++ jenkins-jobs/config/couchstore/ASan-UBSan.groovy
@@ -5,20 +5,4 @@ return {
     //   either link runtime to your application or manually preload it with
     //   LD_PRELOAD.
     CTEST_ARGS="--exclude-regex couchstore-purge|couchstore-rewind|couchstore-changecount"
-
-    // On master branch, there are 4 instances of the following warning from
-    // view-engine code:
-    //     runtime error: member call on address 0x60d000000380 which does not point to an object of type 'v8::Platform'
-    //
-    //     0x60d000000380: note: object has invalid vptr
-    //      01 00 00 00  e0 9f dd dc 10 7f 00 00  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  00 00 00 00
-    //                   ^~~~~~~~~~~~~~~~~~~~~~~
-    //                   invalid vptr
-    //         #0 0x4ebce8 in std::default_delete::operator()(v8::Platform*) const /opt/gcc-10.2.0/lib/gcc/x86_64-pc-linux-gnu/10.2.0/../../../../include/c++/10.2.0/bits/unique_ptr.h:85:2
-    //         #1 0x4eadcd in std::unique_ptr >::~unique_ptr() /opt/gcc-10.2.0/lib/gcc/x86_64-pc-linux-gnu/10.2.0/../../../../include/c++/10.2.0/bits/unique_ptr.h:361:4
-    //         #2 0x7f10d7eb8160 in __run_exit_handlers /build/glibc-S9d2JN/glibc-2.27/stdlib/exit.c:108
-    //         #3 0x7f10d7eb8259 in exit /build/glibc-S9d2JN/glibc-2.27/stdlib/exit.c:139
-    //         #4 0x7f10d7e96bfd in __libc_start_main /build/glibc-S9d2JN/glibc-2.27/csu/../csu/libc-start.c:344
-    //         #5 0x438779 in _start (/home/couchbase/jenkins/workspace/couchstore.ASan-UBSan_master/build/couchstore/couchstore_mapreduce-builtin-test+0x438779)
-    WARNING_THRESHOLD=4
 }
