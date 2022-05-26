f1111c5482383bf5e016ae152187fd5d99a81625
Dave Rigby <daver@couchbase.com>
2022-05-26 16:13:04 +0100

    CBD-4729: macOS: remove CommandLineTools_11.3.1 from PATH
    For C++20 (ish) support we require at least XCode 12.1. This is
    installed in the default path and hence we no longer want to prepend
    /Library/Developer/CommandLineTools_11.3.1 to our path as that will
    cause us to continue to pickup XCode 11.3.1 for CV builds.
    
    Change-Id: Ic845ea4136d380ae55f70f7fe3e0709bdc74b0dd
    Reviewed-on: https://review.couchbase.org/c/server-cv/+/175402
    Tested-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Richard de Mellow <richard.demellow@couchbase.com>

--- jenkins-jobs/config/common/macos.groovy
+++ jenkins-jobs/config/common/macos.groovy
@@ -1,7 +1,3 @@
 return {
     CMAKE_ARGS= "${CMAKE_ARGS} -DBUILD_ENTERPRISE=1"
-
-    // For C++17 support we require at least XCode 11. This is installed in
-    // a non-default path on the macOS CV machines.
-    PATH="/Library/Developer/CommandLineTools_11.3.1/usr/bin:${PATH}"
 }
