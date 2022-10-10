55f0c2142b38f33fdcfc77c167c753b340560269
Dave Rigby <daver@couchbase.com>
2022-10-10 15:11:19 +0100

    Disable Unity build for kv_engine/linux
    This allows us to catch unity-specific issues (missing #header
    includes which we get 'for free' when building as a Unity build).
    
    Change-Id: I8949bd556ba51845e3bfb5ad6c7b742250ea5262
    Reviewed-on: https://review.couchbase.org/c/server-cv/+/181045
    Tested-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: James H <james.harrison@couchbase.com>

--- jenkins-jobs/config/kv_engine/linux.groovy
+++ jenkins-jobs/config/kv_engine/linux.groovy
@@ -2,5 +2,10 @@ return {
 CMAKE_ARGS="${CMAKE_ARGS} " +
            "-DPHOSPHOR_DISABLE=ON"
 
+// Disable Unity build on this job (the fastest job which compiles EE code) so
+// we have coverage of non-unity in CV, to flag any non-unity specific issues
+// like missing header #includes.
+CMAKE_ARGS="${CMAKE_ARGS} -DCB_UNITY_BUILD=OFF"
+
 CTEST_ARGS="--exclude-regex memcached-spdlogger-test"
 }
