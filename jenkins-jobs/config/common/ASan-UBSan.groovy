62d59219a3c1a5f448b9ba7b13f0c3a2c49d5143
Trond Norbye <trond.norbye@couchbase.com>
2024-08-23 13:00:19 +0000

    Revert "MB-49922: Upgrade ASAN/UBSAN cv job from clang-9 to clang-15"
    This reverts commit f75a3fc919a230720f6e0c785e919970d286bd27.
    
    Reason for revert: The debug variable test failed miserably
    
    Change-Id: If932306dd7a7d3fa268ad266eae69dd5412f1fde
    Reviewed-on: https://review.couchbase.org/c/server-cv/+/214630
    Tested-by: Trond Norbye <trond.norbye@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@couchbase.com>

--- jenkins-jobs/config/common/ASan-UBSan.groovy
+++ jenkins-jobs/config/common/ASan-UBSan.groovy
@@ -1,10 +1,20 @@
 return {
-    CC="clang-15"
-    CXX="clang++-15"
-    CFLAGS="--gcc-toolchain=/opt/gcc-13.2.0 -Wl,-rpath,/opt/gcc-13.2.0/lib64"
-    CXXFLAGS="--gcc-toolchain=/opt/gcc-13.2.0 -Wl,-rpath,/opt/gcc-13.2.0/lib64"
-    PATH="/opt/clang-15.0.7/bin:" + "${PATH}"
+    CC="clang-9"
+    CXX="clang++-9"
+    // As of Cheshire-Cat we have switch to GCC 10, which is installed in
+    // parallel with GCC-7 on the standard build image. To use the newer C++
+    // standard library from GCC-10 we need to explicitly tell clang where to
+    // find it.
+    CFLAGS="--gcc-toolchain=/opt/gcc-10.2.0 -Wl,-rpath,/opt/gcc-10.2.0/lib64"
+    CXXFLAGS="--gcc-toolchain=/opt/gcc-10.2.0 -Wl,-rpath,/opt/gcc-10.2.0/lib64"
+
+    // Add llvm-9's bin dir to path, so `llvm-symbolizer' can be found (we
+    // cannot use the versioned suffix one - llvm-symbolizer-9' - as the
+    // binary must be named 'llvm-symbolizer').
+    PATH="${PATH}" + ":/usr/lib/llvm-9/bin/"
+
     CMAKE_ARGS="-DBUILD_ENTERPRISE=1"
+
     ENABLE_ADDRESSSANITIZER=1
     ENABLE_UNDEFINEDSANITIZER=1
     UBSAN_OPTIONS="print_stacktrace=1 log_path=sanitizers.log suppressions=${WORKSPACE}/tlm/ubsan.suppressions"
