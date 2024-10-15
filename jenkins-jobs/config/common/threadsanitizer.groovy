5ccc55f52406e6ab1d35c3d2d377628b2c5d0baf
Trond Norbye <trond.norbye@gmail.com>
2024-10-15 12:24:40 +0200

    Use the default gcc for thread sanitizer (gcc-13.2.0)
    Change-Id: Ic4162d723769f7c96db7344608a28f37ab7cbd00
    Reviewed-on: https://review.couchbase.org/c/server-cv/+/217936
    Tested-by: Trond Norbye <trond.norbye@couchbase.com>
    Reviewed-by: Jim Walker <jim@couchbase.com>

--- jenkins-jobs/config/common/threadsanitizer.groovy
+++ jenkins-jobs/config/common/threadsanitizer.groovy
@@ -1,12 +1,3 @@
 return {
-    CC="clang-9"
-    CXX="clang++-9"
-    // As of Cheshire-Cat we have switch to GCC 10, which is installed in
-    // parallel with GCC-7 on the standard build image. To use the newer C++
-    // standard library from GCC-10 we need to explicitly tell clang where to
-    // find it.
-    CFLAGS="--gcc-toolchain=/opt/gcc-10.2.0 -Wl,-rpath,/opt/gcc-10.2.0/lib64"
-    CXXFLAGS="--gcc-toolchain=/opt/gcc-10.2.0 -Wl,-rpath,/opt/gcc-10.2.0/lib64"
-
     ENABLE_THREADSANITIZER=1
 }
