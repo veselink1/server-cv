b626d0bcf9a308fb8ffb93e58a61b54d38589ff9
Trond Norbye <trond.norbye@gmail.com>
2025-01-15 17:03:15 +0100

    Flip kv_engine's linux build to clang 18.1.8
    This is a prepration to flip the sanitizer builds from clang to gcc
    
    Change-Id: I7c4f2922df1bf7108f39842abb23d368d5d1cba0
    Reviewed-on: https://review.couchbase.org/c/server-cv/+/221936
    Tested-by: Trond Norbye <trond.norbye@couchbase.com>
    Reviewed-by: Mohammad Zaeem <mohammad.zaeem@couchbase.com>

--- jenkins-jobs/config/kv_engine/linux.groovy
+++ jenkins-jobs/config/kv_engine/linux.groovy
@@ -1,8 +1,13 @@
 return {
-// Disable Unity build on this job (the fastest job which compiles EE code) so
-// we have coverage of non-unity in CV, to flag any non-unity specific issues
-// like missing header #includes.
-CMAKE_ARGS="${CMAKE_ARGS} -DCB_UNITY_BUILD=OFF"
+    CC="clang-18"
+    CXX="clang++-18"
+    CFLAGS="--gcc-toolchain=/opt/gcc-13.2.0 -Wl,-rpath,/opt/gcc-13.2.0/lib64"
+    CXXFLAGS="--gcc-toolchain=/opt/gcc-13.2.0 -Wl,-rpath,/opt/gcc-13.2.0/lib64"
+    PATH="/opt/clang-18.1.8/bin:" + "${PATH}"
 
-CTEST_ARGS="--exclude-regex memcached-spdlogger-test"
+    // Disable Unity build on this job (the fastest job which compiles EE code) so
+    // we have coverage of non-unity in CV, to flag any non-unity specific issues
+    // like missing header #includes.
+    CMAKE_ARGS="${CMAKE_ARGS} -DCB_UNITY_BUILD=OFF"
+    CTEST_ARGS="--exclude-regex memcached-spdlogger-test"
 }
