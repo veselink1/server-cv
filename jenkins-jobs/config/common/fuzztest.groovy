bb53d53426b1b96c10c937d3b3c59b5383f4b06b
Vesko Karaganev <vesko.karaganev@couchbase.com>
2025-07-07 17:25:53 +0100

    MB-67369: Add weekly fuzztest and nightly code coverage jobs
    Converted from the script-based jobs to pipelines.
    
    Change-Id: I443b557347e07ca2e40a3c6393741daa52b1712a

---
+++ jenkins-jobs/config/common/fuzztest.groovy
@@ -0,0 +1,16 @@
+return {
+    // Run Saturday, sometime between 12:00 and 24:00
+    CRON_SCHEDULE="H H(12-23) * * 6"
+    // Enable code coverage for fuzz tests.
+    ENABLE_CODE_COVERAGE=true
+    // Add GCC to the PATH so the gcov executable is found.
+    PATH="/opt/gcc-13.2.0/bin:${PATH}"
+    CC="/opt/clang-18.1.8/bin/clang-18"
+    CXX="/opt/clang-18.1.8/bin/clang++-18"
+    CFLAGS="--gcc-toolchain=/opt/gcc-13.2.0 -Wl,-rpath,/opt/gcc-13.2.0/lib64"
+    CXXFLAGS="--gcc-toolchain=/opt/gcc-13.2.0 -Wl,-rpath,/opt/gcc-13.2.0/lib64"
+    CMAKE_ARGS="${CMAKE_ARGS} -DCB_ADDRESSSANITIZER=ON -DFUZZTEST_FUZZING_MODE=ON"
+    // Time budget for fuzzing (per test).
+    FLAGS_fuzz_for="10m"
+    CTEST_ARGS="--tests-regex ^fuzztest\\."
+}
