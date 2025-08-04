554b538a3178b13c32383e16c5f2b4aca5669111
Vesko Karaganev <vesko.karaganev@couchbase.com>
2025-08-04 11:41:25 +0100

    MB-67369: Run fuzztests daily for short time, but longer weekly
    Running the tests nightly ensures we can find out when we're broken
    sooner.
    
    Change-Id: I150152fcfe11da9941812657807055d06d8e9be9
    Reviewed-on: https://review.couchbase.org/c/server-cv/+/231888
    Reviewed-by: Faizan Alam <faizan.alam@couchbase.com>
    Tested-by: Vesko Karaganev <vesko.karaganev@couchbase.com>

--- jenkins-jobs/config/common/fuzztest.groovy
+++ jenkins-jobs/config/common/fuzztest.groovy
@@ -1,12 +1,17 @@
+import java.time.*
 return {
-    // Run Saturday, sometime between 12:00 and 24:00
-    CRON_SCHEDULE="H H(12-23) * * 6"
+    // Every day, sometime between 00:00 and 05:00
+    CRON_SCHEDULE="H H(0-5) * * *"
     CC="/opt/clang-18.1.8/bin/clang-18"
     CXX="/opt/clang-18.1.8/bin/clang++-18"
     CFLAGS="--gcc-toolchain=/opt/gcc-13.2.0 -Wl,-rpath,/opt/gcc-13.2.0/lib64"
     CXXFLAGS="--gcc-toolchain=/opt/gcc-13.2.0 -Wl,-rpath,/opt/gcc-13.2.0/lib64"
     CMAKE_ARGS="${CMAKE_ARGS} -DCB_ADDRESSSANITIZER=ON -DFUZZTEST_FUZZING_MODE=ON"
     // Time budget for fuzzing (per test).
-    FLAGS_fuzz_for="10m"
+    if (LocalDate.now().getDayOfWeek() == DayOfWeek.SATURDAY) {
+        FLAGS_fuzz_for="10m"
+    } else {
+        FLAGS_fuzz_for="10s"
+    }
     CTEST_ARGS="--tests-regex ^fuzztest\\."
 }
